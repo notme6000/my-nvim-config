87631684053da35f7c224440d31a881c04ada9fd
notme6000 <sureshjithu9400@gmail.com>
2026-05-19 10:51:26 +0530

    backup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,14 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+  lazy = false,
 	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			ensure_installed = { "lua", "c" },
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+--    config = function()
+--		local config = require("nvim-treesitter.configs")
+--		config.setup({
+--			auto_install = true,
+--			ensure_installed = { "lua", "c" },
+--			highlight = { enable = true },
+--			indent = { enable = true },
+--		})
+--	end,
 }
